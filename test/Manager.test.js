b4e2d5e334353ec8c5cfbeff59577fd86ac0f901
STadds <staddi@gmail.com>
2020-04-12 15:43:13 -0400

    updated Manager to pass tests

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -16,5 +16,5 @@ test('getRole() should return "Manager"', () => {
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+  expect(e.getOffice()).toBe(testValue);
 });
