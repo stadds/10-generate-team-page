d9963f75424007210c67b130976bd1ad1302d6e3
STadds <staddi@gmail.com>
2020-04-18 14:32:18 -0400

    updating app.js

--- app.js
+++ app.js
@@ -114,14 +114,11 @@ async function getPrompts() {
             console.log(answer);
             // console.log(answer.nextStep);
 
-            // const employee = await inquirer.prompt(employeePrompt);
-            //console.log(answer)
-
             //console.log(employee);
 
             switch (answer.nextStep) {
                 case "Engineer":
-                    console.log("ENGINEER QS");
+                    // console.log("ENGINEER QS");
                     await inquirer.prompt([...employeePrompt, ...engineerPrompt]).then(answers => {
                         //console.log(answers);
                         let newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
@@ -129,7 +126,7 @@ async function getPrompts() {
                     })
                     break;
                 case "Manager":
-                    console.log("MANAGER Qs");
+                    // console.log("MANAGER Qs");
                     await inquirer.prompt([...employeePrompt, ...managerPrompt]).then(answers => {
                         // console.log(answers);
                         let newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
@@ -138,7 +135,7 @@ async function getPrompts() {
                     break;
 
                 case "Intern":
-                    console.log("INTERN Qs");
+                    // console.log("INTERN Qs");
                     await inquirer.prompt([...employeePrompt, ...internPrompt]).then(answers => {
                         // console.log(answers);
                         let newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
@@ -170,34 +167,6 @@ async function getPrompts() {
 function init() {
     console.log("TESTING");
     getPrompts();
-    //console.log("testing 2.0");
-
 }
 
 init();
-
-
-/*
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-*/
